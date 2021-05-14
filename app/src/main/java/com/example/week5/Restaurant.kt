771caeee722eb5d34d47239f1bb951c4bbf70a65
DESKTOP-FJAIC4F\HOANG TUNG <hoangtung2579@gmail.com>
2021-05-14 21:04:18 +0700

    week 6

--- app/src/main/java/com/example/week5/Restaurant.kt
+++
@@ -1,5 +0,0 @@
-package com.example.week5
-
-data class Restaurant(var id:Int,var name:String,var address:String,var picturePath:String,var isFavorite:Boolean) {
-    
-}
--- app/src/main/java/com/example/week5/fragment/NowPlayingMovieFragment.kt
+++ app/src/main/java/com/example/week5/fragment/NowPlayingMovieFragment.kt
@@ -1,7 +1,6 @@
-package com.example.week5
+package com.example.week5.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,29 +8,34 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.week5.GRID_LAYOUT
+import com.example.week5.LINEAR_LAYOUT
+import com.example.week5.R
+import com.example.week5.adapter.NowPlayingMovieAdapter
+import com.example.week5.movie.Movie
 
-class TopRestaurantFragment(val topRestaurantAdapter: TopRestaurantAdapter):Fragment() {
+class NowPlayingMovieFragment(val nowPlayingMovieAdapter: NowPlayingMovieAdapter):Fragment() {
     private lateinit var rcList : RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_top_restaurant,container,false)
+        val view = inflater.inflate(R.layout.fragment_top_rated_movie,container,false)
         rcList = view.findViewById<RecyclerView>(R.id.rcTop)
         rcList.layoutManager = LinearLayoutManager(this.context)
-        rcList.adapter = topRestaurantAdapter
+        rcList.adapter = nowPlayingMovieAdapter
         return view
     }
     fun changeLayout(viewType:Int){
         when(viewType){
-            LINEAR_LAYOUT->{
+            LINEAR_LAYOUT ->{
                 rcList.layoutManager = LinearLayoutManager(this.context)
-                topRestaurantAdapter.changeViewType(LINEAR_LAYOUT)
+                nowPlayingMovieAdapter.changeViewType(LINEAR_LAYOUT)
             }
-            GRID_LAYOUT->{
+            GRID_LAYOUT ->{
                 rcList.layoutManager = GridLayoutManager(this.context,2)
-                topRestaurantAdapter.changeViewType(GRID_LAYOUT)
+                nowPlayingMovieAdapter.changeViewType(GRID_LAYOUT)
             }
             else ->{
                 print("error")
